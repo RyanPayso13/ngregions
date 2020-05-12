9ec7fab1e3e61e5f68c1059b307988aec091f067
Ryan Pays <ryan.pays@gmail.com>
2020-05-12 14:54:58 +0100

    move the store under app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
-import * as fromRegion from '../store/reducers/region.reducer';
-import * as fromCountry from '../store/reducers/country.reducer';
+import * as fromRegion from './store/reducers/region.reducer';
+import * as fromCountry from './store/reducers/country.reducer';
 
 import { DetailsContainerComponent } from './components/country/details-container/details-container.component';
 import { DetailsUiComponent } from './components/country/details-ui/details-ui.component';
